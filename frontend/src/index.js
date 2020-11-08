2b16974421880aa9ffccc3459dd550edafb6d73a
geliyangupenn <70827733+geliyangupenn@users.noreply.github.com>
2020-11-08 17:17:37 -0500

    login page
    log in page for now.
    Need to modify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginPage from './userLoginAuth';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <LoginPage/>
   </React.StrictMode>,
   document.getElementById('root')
 );
