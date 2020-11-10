750ba1532b5909812a923ffac0d0b67b51bc307d
geliyangupenn <70827733+geliyangupenn@users.noreply.github.com>
2020-11-09 22:03:18 -0500

    changed name of js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import history from './history';
 //router
 import { Router, Route, Switch } from 'react-router-dom';
 import UserRegistration from './UserRegistration';
-import LoginPage from './userLoginAuth';
+import LoginPage from './LoginPage';
 import ProfilePage from './views/ProfilePage';
 
 const App = () => {
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import LoginPage from './userLoginAuth';
+import LoginPage from './LoginPage';
 
 import UserRegistration from './UserRegistration';
 
